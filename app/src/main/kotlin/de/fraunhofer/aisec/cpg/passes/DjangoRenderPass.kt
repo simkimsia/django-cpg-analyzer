3294ca43f1b7113a8d24ccca540dd4dfe20f4204
KimSia, Sim <245021+simkimsia@users.noreply.github.com>
2025-01-20 19:45:56 +0800

    refactor: changed the DjangoRenderPass
    - Look specifically for render or render_to_response calls
    - Create a TemplateRenderer overlay node for each render call
    - Extract the template path from the first argument (accounting for different argument positions in render vs render_to_response)
    - Set up the connection between the overlay node and the underlying call expression
    - Added a placeholder for context variable extraction (which will need dictionary analysis to implement fully)
    
    Signed-off-by: KimSia, Sim <245021+simkimsia@users.noreply.github.com>

--- app/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/DjangoRenderPass.kt
+++ app/src/main/kotlin/de/fraunhofer/aisec/cpg/passes/DjangoRenderPass.kt
@@ -4,6 +4,7 @@ import de.fraunhofer.aisec.cpg.TranslationResult
 import de.fraunhofer.aisec.cpg.TranslationContext
 import de.fraunhofer.aisec.cpg.graph.Node
 import de.fraunhofer.aisec.cpg.graph.statements.expressions.CallExpression
+import de.fraunhofer.aisec.cpg.graph.statements.expressions.Literal
 import de.fraunhofer.aisec.cpg.helpers.SubgraphWalker
 import de.fraunhofer.aisec.cpg.frontends.python.PythonLanguageFrontend
 import de.fraunhofer.aisec.cpg.passes.configuration.RequiredFrontend
@@ -20,7 +21,28 @@ class DjangoRenderPass(ctx: TranslationContext) : TranslationResultPass(ctx) {
                 println("  Analyzing translation unit: ${tu.name}")
                 val nodes = SubgraphWalker.flattenAST(tu)
                 nodes.filterIsInstance<CallExpression>().forEach { call ->
-                    println("    Found call: ${call.name} at line ${call.location?.region?.startLine}")
+                    // Look for render or render_to_response calls
+                    if (call.name.lastPartsMatch("render") || call.name.lastPartsMatch("render_to_response")) {
+                        println("    Found render call: ${call.name} at line ${call.location?.region?.startLine}")
+
+                        // Create a new TemplateRenderer overlay node
+                        val templateRenderer = TemplateRenderer()
+
+                        // Try to extract template path from the first argument
+                        if (call.arguments.isNotEmpty()) {
+                            val templateArg = call.arguments[if (call.name.lastPartsMatch("render")) 1 else 0]
+                            if (templateArg is Literal<*>) {
+                                templateRenderer.templatePath = templateArg.value.toString()
+                            }
+                        }
+
+                        // Try to extract context variables from the context dictionary
+                        val contextArg = call.arguments.getOrNull(if (call.name.lastPartsMatch("render")) 2 else 1)
+                        // TODO: Implement context variable extraction when we have dictionary analysis
+
+                        // Set the underlying node to connect the overlay
+                        templateRenderer.underlyingNode = call
+                    }
                 }
             }
         }
